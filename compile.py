d0d2b76feab1905ccb3ed9f45615e125257bec12
gentlegiantJGC <james_clare1@yahoo.co.uk>
2018-09-14 17:00:20 +0100

    added resolution variants, converted to jpg and added blockworks marketplace page

--- compile.py
+++ compile.py
@@ -1,4 +1,5 @@
 import os
+from PIL import Image
 os.chdir(r'C:\Users\james_000\Documents\GitHub\gentlegiantJGC.github.io')
 with open('main.jgc', 'r') as mainf:
 	main = mainf.read()
@@ -10,37 +11,56 @@ else:
 	dataDivNum = dataDivNum[0]
 dataTabNum = len(main[dataDivNum])-len(main[dataDivNum].lstrip('\t'))
 
-# for dirpath, dirnames, filenames in os.walk('.'):
-	# if dirpath == '.'
-
 for dirpath, dirnames, filenames in os.walk('.'):
-	if 'addon' in dirpath:
-		continue
-	if dirpath.split(os.sep)[-1] in ['css','.git','mainIcons']:
+	if 'addon' in dirpath or dirpath.split(os.sep)[-1] in ['css','.git','mainIcons']:
 		continue
 	elif 'list.jgc' in filenames:
-		with open(dirpath + os.sep + 'list.jgc', 'r') as dataf:
+		with open(os.path.join(dirpath, 'list.jgc'), 'r') as dataf:
 			data = dataf.read()
 		data = data.split('\n')
-		element = dataTabNum * '\t'+'<div class="data">\n'
+		element = '{}<div class="data">\n'.format(dataTabNum * '\t')
 		for n in range(len(data)):
 			if n%2 == 0:
 				side = 'left'
 			else:
 				side = 'right'
 			path, title, desc = data[n].split(';')
-			element += (dataTabNum+1) * '\t'+'<div class="dataline'+side+'">\n'
-			element += (dataTabNum+2) * '\t'+'<a class="elementlink" href="/'+path+'"></a>\n'
-			element += (dataTabNum+2) * '\t'+'<img class="elementimg" src="/'+path+'/thumb.png" alt="'+path+'" />\n'
-			element += (dataTabNum+2) * '\t'+'<div class="elementdescript">\n'
+			element += '{}<div class="dataline{}">\n'.format((dataTabNum+1) * '\t', side)
+			element += '{}<a class="elementlink" href="/{}"></a>\n'.format((dataTabNum+2) * '\t', path)
+			if 'thumb.jpg' in os.listdir('./{}'.format(path)):
+				thumbs = [a for a in os.listdir('./{}'.format(path)) if a.startswith('thumb') and a.endswith('.jpg')]
+				if len(thumbs) > 1:
+					for thumb in thumbs:
+						if thumb != 'thumb.jpg':
+							os.remove(os.path.join(path, thumb))
+				thumbs = []
+				thumb = Image.open(os.path.join(path, 'thumb.jpg'))
+				width, height = thumb.size
+				while width > 100:
+					thumb2 = thumb.copy()
+					thumb2.thumbnail((width, height))
+					thumb2.save(os.path.join(path, 'thumb{}.jpg'.format(width)), "JPEG", quality=60, optimize=True)
+					thumbs.append(['thumb{}.jpg'.format(width), width])
+					width = int(width/1.5)
+				thumbs.reverse()
+				if len(thumbs) > 0:
+					element += '{}<img class="elementimg" src="/{}/thumb.jpg" alt="{}" srcset="{}" />\n'.format(
+						(dataTabNum + 3) * '\t',
+						path, path,
+						', '.join(['/{}/{} {}w'.format(path, a[0], a[1]*2) for a in thumbs]))
+				else:
+					raise Exception('failed creating thumbs at {}'.format(path))
+			else:
+				raise Exception('no thumb.jpg given at {}'.format(path))
+			element += '{}<div class="elementdescript">\n'.format((dataTabNum+2) * '\t')
 			
-			element += (dataTabNum+3) * '\t'+'<b class="descriptTitle">'+title+'</b>\n'
-			element += (dataTabNum+3) * '\t'+'<p class="descriptMain">'+desc+'</p>\n'
+			element += '{}<b class="descriptTitle">{}</b>\n'.format((dataTabNum+3) * '\t', title)
+			element += '{}<p class="descriptMain">{}</p>\n'.format((dataTabNum+3) * '\t', desc)
 
-			element += (dataTabNum+2) * '\t'+'</div>\n'
+			element += '{}</div>\n'.format((dataTabNum+2) * '\t')
 			
-			element += (dataTabNum+1) * '\t'+'</div>\n'
-		element += dataTabNum * '\t'+'</div>'
+			element += '{}</div>\n'.format((dataTabNum+1) * '\t')
+		element += '{}</div>'.format(dataTabNum * '\t')
 		
 		maintemp = main
 		maintemp[dataDivNum] = element
---
+++ mipmaps.py
@@ -0,0 +1,30 @@
+import os
+from PIL import Image
+
+def createMipMap(imgPath):
+	if os.path.isfile(imgPath):
+		imgFolder, imgExtension = os.path.splitext(imgPath)
+		imgFolder = imgFolder.split(os.sep)
+		imgName = imgFolder[-1]
+		path = os.sep.join(imgFolder[:-1])
+		path = os.path.relpath(path).replace('\\', '/')
+
+		thumbs = []
+		thumb = Image.open(imgPath)
+		width, height = thumb.size
+		while width > 100:
+			thumb2 = thumb.copy()
+			thumb2.thumbnail((width, height))
+			thumb2.save(os.path.join(path, '{}{}.jpg'.format(imgName, width)), "JPEG", quality=75, optimize=True)
+			thumbs.append(['{}{}.jpg'.format(imgName, width), width])
+			width = int(width / 1.5)
+		thumbs.reverse()
+		if len(thumbs) > 0:
+			print '<img src="/{}/{}{}" alt="{}" srcset="{}" />\n'.format(path, imgName, imgExtension, path,
+				', '.join(['/{}/{} {}w'.format(path, a[0], a[1]) for a in thumbs]))
+		else:
+			raise Exception('failed creating thumbs at {}'.format(path))
+	else:
+		print '"{}" is not a valid file'.format(imgPath)
+
+createMipMap(r"C:\Users\james_000\Documents\GitHub\gentlegiantJGC.github.io\blockworks\marketplace\cover.jpg")
