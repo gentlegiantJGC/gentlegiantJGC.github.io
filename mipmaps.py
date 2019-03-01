0dd0a114a515c948f29f185c39fe8ee82aaa77f5
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-03-01 12:55:47 +0000

    updated mipmap script to python 3.7

--- mipmaps.py
+++ mipmaps.py
@@ -1,30 +1,30 @@
 import os
 from PIL import Image
 
-def createMipMap(imgPath):
-	if os.path.isfile(imgPath):
-		imgFolder, imgExtension = os.path.splitext(imgPath)
-		imgFolder = imgFolder.split(os.sep)
-		imgName = imgFolder[-1]
-		path = os.sep.join(imgFolder[:-1])
+def create_mip_map(img_path):
+	if os.path.isfile(img_path):
+		img_folder, img_extension = os.path.splitext(img_path)
+		img_folder = img_folder.split(os.sep)
+		img_name = img_folder[-1]
+		path = os.sep.join(img_folder[:-1])
 		path = os.path.relpath(path).replace('\\', '/')
 
 		thumbs = []
-		thumb = Image.open(imgPath)
+		thumb = Image.open(img_path)
 		width, height = thumb.size
 		while width > 100:
 			thumb2 = thumb.copy()
 			thumb2.thumbnail((width, height))
-			thumb2.save(os.path.join(path, '{}{}.jpg'.format(imgName, width)), "JPEG", quality=75, optimize=True)
-			thumbs.append(['{}{}.jpg'.format(imgName, width), width])
+			thumb2.save(os.path.join(path, '{}{}.jpg'.format(img_name, width)), "JPEG", quality=75, optimize=True)
+			thumbs.append(['{}{}.jpg'.format(img_name, width), width])
 			width = int(width / 1.5)
 		thumbs.reverse()
 		if len(thumbs) > 0:
-			print '<img src="/{}/{}{}" alt="{}" srcset="{}" />\n'.format(path, imgName, imgExtension, path,
-				', '.join(['/{}/{} {}w'.format(path, a[0], a[1]) for a in thumbs]))
+			print('<img src="/{}/{}{}" alt="{}" srcset="{}" />\n'.format(path, img_name, img_extension, path,
+				', '.join(['/{}/{} {}w'.format(path, a[0], a[1]) for a in thumbs])))
 		else:
 			raise Exception('failed creating thumbs at {}'.format(path))
 	else:
-		print '"{}" is not a valid file'.format(imgPath)
+		print('"{}" is not a valid file'.format(img_path))
 
-createMipMap(r"C:\Users\james_000\Documents\GitHub\gentlegiantJGC.github.io\gamemodeone\marketplace\cover.jpg")
+create_mip_map(r"C:\Users\james_000\Documents\GitHub\gentlegiantJGC.github.io\GCode-Sequencer\cover.jpg")
